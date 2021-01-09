d0d8a43d8be73832ab6753848d61ead2b9195a2a
k.mendji@yahoo.de <MONde1914{}1938>
2021-01-09 12:09:33 +0100

    configure pom

--- src/main/java/controller/Test.java
+++ src/main/java/controller/Test.java
@@ -1,4 +1,23 @@
 package controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Test {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello Maven");
+        Properties properties = new Properties();
+        InputStream inputStream = null;
+        try {
+            inputStream = Test.class.getResourceAsStream("/info.properties");
+            properties.load(inputStream);
+        }finally {
+            if(inputStream != null)
+                inputStream.close();
+        }
+        System.out.println("la version Java untilisee est: "
+                + properties.getProperty("java.version","?"));
+    }
 }
---
+++ src/test/java/HalloworldTest.java
@@ -0,0 +1,10 @@
+public class HalloworldTest {
+
+    public void testHalloworld1() {
+        System.out.println("Hello world 1");
+    }
+
+    public void testHalloworld2() {
+        System.out.println("Hello world 2");
+    }
+}
